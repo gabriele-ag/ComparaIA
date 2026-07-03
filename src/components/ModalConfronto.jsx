a541529d248d159213793d5c052080c8ec3225d6
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2026-07-03 08:31:30 +0200

    feat: updated modalconfronto

--- src/components/ModalConfronto.jsx
+++ src/components/ModalConfronto.jsx
@@ -1,162 +1,166 @@
-import "./CSS/ModalConfronto.css"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 import ReactDOM from "react-dom";
+import styles from "./CSS/modalconfronto.module.css"; // Aggiornato all'uso dei moduli CSS in camelCase
 
-import styles from "./CSS/modalmodifica.module.css"
-
-const ModalConfronto = ({isOpen, onClose, items}) => {
+const ModalConfronto = ({ isOpen, onClose, items }) => {
 
     // È aperto?
-    if(!isOpen) return null
+    if (!isOpen) return null;
 
     // Creo un massimo di oggetti comparabili
-    const maxItemsComparable = items.slice(0, 3)
+    const maxItemsComparable = items.slice(0, 3);
 
     // Rating da numerico trasformato in icone
     const ratingIcon = (rating) => {
-        const icons = []
-        const fullIcon = Math.floor(rating)
-        const halfIcon = rating % 1 >= 0.5
-        const emptyIcon = 5 - fullIcon - (halfIcon ? 1 : 0)
+        const icons = [];
+        const fullIcon = Math.floor(rating);
+        const halfIcon = rating % 1 >= 0.5;
+        const emptyIcon = 5 - fullIcon - (halfIcon ? 1 : 0);
 
         for (let i = 0; i < fullIcon; i++) {
-            icons.push(<i key={`full-${i}`} className="fa-solid fa-star"></i>)
+            icons.push(<i key={`full-${i}`} className="fa-solid fa-star"></i>);
         }
 
         if (halfIcon) {
-            icons.push(<i key={halfIcon} className="fa-solid fa-star-half-stroke"></i>)
+            icons.push(<i key="half" className="fa-solid fa-star-half-stroke"></i>);
         }
 
         for (let i = 0; i < emptyIcon; i++) {
-            icons.push(<i key={`empty-${i}`} className="fa-regular fa-star"></i>)
+            icons.push(<i key={`empty-${i}`} className="fa-regular fa-star"></i>);
         }
 
-        return icons
-    }
+        return icons;
+    };
 
     const modalContent = (
-        
-        <div className="modal-overlay">
-            <div className="modal-content">
-                <div className="modal-box-title">
-                    <button className="modal-btn-close" onClick={onClose}><i className="fa-solid fa-xmark"></i></button>
+        <div className={styles.modalOverlay}>
+            <div className={styles.modalContent}>
+                <div className={styles.modalBoxTitle}>
+                    <button className={styles.modalBtnClose} onClick={onClose}>
+                        <i className="fa-solid fa-xmark"></i>
+                    </button>
                 </div>
-                <h3 className="modal-title">Risultati Comparazione</h3>
-                <div className="modal-ai modal-table-wrapper">                   
-                            <table className="modal-compare-table">
-                            <thead>
-                                <tr>
+                <h3 className={styles.modalTitle}>Risultati Comparazione</h3>
+                <div className={styles.modalTableWrapper}>                   
+                    <table className={styles.modalCompareTable}>
+                        <thead>
+                            <tr>
                                 <th></th>
                                 {maxItemsComparable.map((curElem) => (
                                     <th key={curElem.id}>{curElem.title}</th>
                                 ))}
-                                </tr>
-                            </thead>
-                            <tbody>
-
-                                <tr>
-                                <td className="modal-feature-title">Rating</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className={styles.modalFeatureTitle}>Rating</td>
                                 {maxItemsComparable.map((curElem) => (
-                                    <td className="modal-rating" key={`rating-${curElem.id}`}>{ratingIcon(curElem.rating)}</td>
+                                    <td className={styles.modalRating} key={`rating-${curElem.id}`}>
+                                        {ratingIcon(curElem.rating)}
+                                    </td>
                                 ))}
-
-                                </tr>
-                                
-                                <tr>
-                                <td className="modal-feature-title">Brand</td>
+                            </tr>
+                            
+                            <tr>
+                                <td className={styles.modalFeatureTitle}>Brand</td>
                                 {maxItemsComparable.map((curElem) => (
-                                    <td key={`desc-${curElem.id}`}>{curElem.brand}</td>
-                                    ))}
-                                </tr>
+                                    <td key={`brand-${curElem.id}`}>{curElem.brand}</td>
+                                ))}
+                            </tr>
 
-                                <tr>
-                                <td className="modal-feature-title">Descrizione</td>
+                            <tr>
+                                <td className={styles.modalFeatureTitle}>Descrizione</td>
                                 {maxItemsComparable.map((curElem) => (
                                     <td key={`desc-${curElem.id}`}>{curElem.description}</td>
-                                    ))}
-                                </tr>
-                                <tr>
-
-                                <td className="modal-feature-title">Usato per</td>
-                                {maxItemsComparable.map((curElem) => (
-                                    <td key={`lang-${curElem.id}`}>{curElem.useCases.join(", ")}</td>
                                 ))}
-                                </tr>
+                            </tr>
+                            
+                            <tr>
+                                <td className={styles.modalFeatureTitle}>Usato per</td>
+                                {maxItemsComparable.map((curElem) => (
+                                    <td key={`useCases-${curElem.id}`}>{curElem.useCases.join(", ")}</td>
+                                ))}
+                            </tr>
 
-                                <tr>
-                                <td className="modal-feature-title">Prezzo</td>
+                            <tr>
+                                <td className={styles.modalFeatureTitle}>Prezzo</td>
                                 {maxItemsComparable.map((curElem) => (
                                     <td key={`price-${curElem.id}`}>{curElem.price}</td>
                                 ))}
-                                </tr>
-                                
-                                <tr>
-                                <td className="modal-feature-title">Privacy</td>
+                            </tr>
+                            
+                            <tr>
+                                <td className={styles.modalFeatureTitle}>Privacy</td>
                                 {maxItemsComparable.map((curElem) => (
                                     <td key={`privacy-${curElem.id}`}>{curElem.privacyLevel}</td>
                                 ))}
-                                </tr>
+                            </tr>
 
-                                <tr>
-                                <td className="modal-feature-title">Lingue supportate</td>
+                            <tr>
+                                <td className={styles.modalFeatureTitle}>Lingue supportate</td>
                                 {maxItemsComparable.map((curElem) => (
                                     <td key={`lang-${curElem.id}`}>{curElem.supportedLanguages.join(", ")}</td>
                                 ))}
-                                </tr>
+                            </tr>
 
-                                <tr>
-                                <td className="modal-feature-title">Piattaforma</td>
+                            <tr>
+                                <td className={styles.modalFeatureTitle}>Piattaforma</td>
                                 {maxItemsComparable.map((curElem) => (
-                                    <td key={`lang-${curElem.id}`}>{curElem.platforms.join(", ")}</td>
+                                    <td key={`platform-${curElem.id}`}>{curElem.platforms.join(", ")}</td>
                                 ))}
-                                </tr>
+                            </tr>
 
-                                <tr>
-                                <td className="modal-feature-title">Integrazione</td>
+                            <tr>
+                                <td className={styles.modalFeatureTitle}>Integrazione</td>
                                 {maxItemsComparable.map((curElem) => (
-                                    <td key={`lang-${curElem.id}`}>{curElem.integrations.join(", ")}</td>
+                                    <td key={`integ-${curElem.id}`}>{curElem.integrations.join(", ")}</td>
                                 ))}
-                                </tr>
+                            </tr>
 
-                                <tr>
-                                <td className="modal-feature-title">Anno di rilascio</td>
+                            <tr>
+                                <td className={styles.modalFeatureTitle}>Anno di rilascio</td>
                                 {maxItemsComparable.map((curElem) => (
-                                    <td key={`lang-${curElem.id}`}>{curElem.releaseYear}</td>
+                                    <td key={`year-${curElem.id}`}>{curElem.releaseYear}</td>
                                 ))}
-                                </tr>
+                            </tr>
 
-                                <tr>
-                                <td className="modal-feature-title">V. Gratuita?</td>
+                            <tr>
+                                <td className={styles.modalFeatureTitle}>V. Gratuita?</td>
                                 {maxItemsComparable.map((curElem) => (
-                                    <td key={`lang-${curElem.id}`}>{curElem.hasFreeVersion === true ? "Sì" : "No"}</td>
+                                    <td key={`free-${curElem.id}`}>
+                                        {curElem.hasFreeVersion === true ? "Sì" : "No"}
+                                    </td>
                                 ))}
-                                </tr>
+                            </tr>
 
-                                <tr>
-                                <td className="modal-feature-title">Sito Web</td>
+                            <tr>
+                                <td className={styles.modalFeatureTitle}>Sito Web</td>
                                 {maxItemsComparable.map((curElem) => (
-                                    <td key={`lang-${curElem.id}`}><Link className="modal-website-ai" to={curElem.website}>{curElem.website}</Link></td>
+                                    <td key={`web-${curElem.id}`}>
+                                        <Link className={styles.modalWebsiteAi} to={curElem.website}>
+                                            {curElem.website}
+                                        </Link>
+                                    </td>
                                 ))}
-                                </tr>
+                            </tr>
 
-                                <tr>
-                                <td className="modal-feature-title">API</td>
+                            <tr>
+                                <td className={styles.modalFeatureTitle}>API</td>
                                 {maxItemsComparable.map((curElem) => (
-                                    <td key={`lang-${curElem.id}`}>{curElem.apiAvailable === true ? "Sì" : "No"}</td>
+                                    <td key={`api-${curElem.id}`}>
+                                        {curElem.apiAvailable === true ? "Sì" : "No"}
+                                    </td>
                                 ))}
-                                </tr>
-
-                                
-                            </tbody>
-                            </table>
-                                       
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
-    )
-
+    );
 
     return ReactDOM.createPortal(modalContent, document.getElementById("modal-root"));
-}
+};
 
-export default ModalConfronto
+export default ModalConfronto;
